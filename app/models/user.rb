87dd28d785cc4d56a9a8e6333a4480a6e971384f
ashleeeeeeyyyyy <puaphee@gmail.com>
2023-04-03 13:21:20 +0800

    Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
-class User < ApplicationRecord
+class User < ApplicationRecord   # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def username
+    email.split('@')[0].capitalize
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'users/index'
   # get 'users/new'
   # get 'users/edit'
@@ -7,6 +8,9 @@ Rails.application.routes.draw do
   # get 'users/destroy'
 
   # post 'users/new', to: 'users#create'
+  devise_scope :user do
+    get "/signout", to: "devise/sessions#destroy", as: :signout
+  end
   
   resources :users, only: [:index, :new, :edit, :create, :update, :destroy]
 end
